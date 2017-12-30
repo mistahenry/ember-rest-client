a36f6a5d19496853253ccc227c029b0986e82eed
William Henry <williamhenry@Williams-MBP.fritz.box>
2017-12-30 15:44:49 +0100

    formatting

--- addon/services/http-client.js
+++ addon/services/http-client.js
@@ -2,43 +2,43 @@ import Ember from 'ember';
 var ajax = require('ic-ajax');
 
 export default Ember.Service.extend({
-	get: function(options){
+    get: function (options) {
         return this.rsvpAjax(this.buildGetOpts(options));
-	},
-    buildGetOpts: function(options){
+    },
+    buildGetOpts: function (options) {
         return this.buildOpts(options, "GET");
     },
-	delete: function(options){
+    delete: function (options) {
         return this.rsvpAjax(this.buildDeleteOpts(options));
-	},
-    buildDeleteOpts: function(options){
+    },
+    buildDeleteOpts: function (options) {
         return this.buildOpts(options, "DELETE");
     },
-	post: function(options) {
+    post: function (options) {
         return this.rsvpAjax(this.buildPostOpts(options));
-	},
-    buildPostOpts: function(options){
+    },
+    buildPostOpts: function (options) {
         return this.buildOpts(options, "POST");
     },
-	put: function(options){
-		return this.rsvpAjax(this.buildPutOpts(options));
-	},
-    buildPutOpts: function(options){
+    put: function (options) {
+        return this.rsvpAjax(this.buildPutOpts(options));
+    },
+    buildPutOpts: function (options) {
         return this.buildOpts(options, "PUT");
     },
-	rsvpAjax: function(options){
+    rsvpAjax: function (options) {
         console.log("rsvpAjax", options);
-		return ajax.request(options.url, options);
-	},
-	buildOpts: function(options, method) {
-        if(!options){
+        return ajax.request(options.url, options);
+    },
+    buildOpts: function (options, method) {
+        if (!options) {
             options = {};
         }
-		var defaultOpts = this.getDefaultOpts();
-		var opts = $.extend({}, defaultOpts, options, {type: method, data: JSON.stringify(options.data)});
-		return opts;
-	},
-    getDefaultOpts: function(){
+        var defaultOpts = this.getDefaultOpts();
+        var opts = $.extend({}, defaultOpts, options, {type: method, data: JSON.stringify(options.data)});
+        return opts;
+    },
+    getDefaultOpts: function () {
         return {};
     }
 });
--- addon/services/rest-client.js
+++ addon/services/rest-client.js
@@ -1,97 +1,97 @@
 import Ember from 'ember';
 export default Ember.Service.extend({
     httpClient: Ember.inject.service(),
-	/**
-	 * I wanted our GET method to be `get` which means we have lost
-	 * access to the Ember.Object `get` method. So if you need to get a property with a
-	 * getter, use Ember.get(this, 'propName')
-	 */
-    get: function(options){
+    /**
+     * I wanted our GET method to be `get` which means we have lost
+     * access to the Ember.Object `get` method. So if you need to get a property with a
+     * getter, use Ember.get(this, 'propName')
+     */
+    get: function (options) {
         return this._makeHttpRequest("get", options);
     },
-    delete: function(options){
-		return this._makeHttpRequest("delete", options);
+    delete: function (options) {
+        return this._makeHttpRequest("delete", options);
     },
-    post: function(options) {
+    post: function (options) {
         return this._makeHttpRequest("post", options);
     },
-    put: function( options){
+    put: function (options) {
         return this._makeHttpRequest("put", options);
     },
-    _makeHttpRequest: function(method, options){
-		var self = this;
+    _makeHttpRequest: function (method, options) {
+        var self = this;
         options.method = method;
-		if(this.requestPermitted(options)){
-			return this._buildAndMakeRequest(method, options);
-		}else{
-		    return this.rejectRequest(options);		
-		}
+        if (this.requestPermitted(options)) {
+            return this._buildAndMakeRequest(method, options);
+        } else {
+            return this.rejectRequest(options);
+        }
     },
     /**
      * Hook to prevent requests based on app specific logic
-    */
-    requestPermitted: function(options){
+     */
+    requestPermitted: function (options) {
         return true;
     },
     /**
      * If requestPermitted returns false, resulting behavior. Should return a
      * promise. Base impl retruns immediately rejecting promise
-    */
-    rejectRequest: function(options){
-        return new Ember.RSVP.Promise(function(resolve,reject){
-            reject(new Error(options.url+": rejected by rest-client"));
+     */
+    rejectRequest: function (options) {
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+            reject(new Error(options.url + ": rejected by rest-client"));
         });
     },
-	_buildAndMakeRequest: function(method,options){
+    _buildAndMakeRequest: function (method, options) {
         this.beforeRequest(options);
-		var defaultedOpts = this._buildOpts(options);
+        var defaultedOpts = this._buildOpts(options);
         var httpClient = Ember.get(this, 'httpClient');
-		var promise = httpClient[method].call(httpClient, defaultedOpts);
+        var promise = httpClient[method].call(httpClient, defaultedOpts);
         this.afterRequest(options);
-		var fns = this._getPromiseFunctions(this, promise, options);
-		this.storePromiseData(promise, options);
-		return promise.then(fns.success,fns.error);
-	},
+        var fns = this._getPromiseFunctions(this, promise, options);
+        this.storePromiseData(promise, options);
+        return promise.then(fns.success, fns.error);
+    },
     /**
      * App specific hook before making a request, unrelated to building the options
      * for the request (better handled by customHttpOptions)
      */
-    beforeRequest: function(options){
+    beforeRequest: function (options) {
         return;
     },
     /**
      * App specific hook after the request is made (not the callback of the request)
      */
-    afterRequest: function(options){
+    afterRequest: function (options) {
         return;
     },
-	/**
-	 * Had to manually provide promise's `then` function both the promise and the rest
-	 * client class due to the way promises are scoped (for some reason `this` is undefined in promise `then` fn)
-	 */
-	_success: function(restClientClass, promise, options){
-		return function(response){
+    /**
+     * Had to manually provide promise's `then` function both the promise and the rest
+     * client class due to the way promises are scoped (for some reason `this` is undefined in promise `then` fn)
+     */
+    _success: function (restClientClass, promise, options) {
+        return function (response) {
             response = restClientClass.customSuccess(response, options);
-			return response;
-		};
-	},
+            return response;
+        };
+    },
     /**
      * App specific success callback. Should return response
      */
-    customSuccess: function(response, options){
+    customSuccess: function (response, options) {
         return response;
     },
-	_error: function(restClientClass, promise, options){
-		var self = this;
-		return function(error){
+    _error: function (restClientClass, promise, options) {
+        var self = this;
+        return function (error) {
             error = restClientClass.customError(error, options);
             restClientClass._rethrowError(error, options);
-		};
-	},
+        };
+    },
     /**
      * App specific custom error handling. Should return error or _rethrowError
      */
-    customError: function(error, options){
+    customError: function (error, options) {
         return error;
     },
     _rethrowError(error, options){
@@ -101,47 +101,46 @@ export default Ember.Service.extend({
         });
         throw error;
     },
-	_getPromiseFunctions: function(restClientClass, promise, options){
-		return {
-			success: this._success(restClientClass, promise, options),
-			error: this._error(restClientClass, promise, options)
-		};
-	},
+    _getPromiseFunctions: function (restClientClass, promise, options) {
+        return {
+            success: this._success(restClientClass, promise, options),
+            error: this._error(restClientClass, promise, options)
+        };
+    },
     headers: {
-         "Content-Type": "application/json"
+        "Content-Type": "application/json"
     },
     defaultOpts: {
-			dataType: 'json',
-			xhrFields: {
-				withCredentials: true
-			}
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true
+        }
     },
-    _getDefaultHttpOpts: function(){
-        var headers = Ember.get(this, 'headers'); 
-		var defaultOpts = this.defaultOpts;
+    _getDefaultHttpOpts: function () {
+        var headers = Ember.get(this, 'headers');
+        var defaultOpts = this.defaultOpts;
         defaultOpts.headers = headers;
         return defaultOpts;
     },
-    customHttpOptions: function(httpOptions, options){
+    customHttpOptions: function (httpOptions, options) {
         return httpOptions;
     },
-	_buildOpts: function(options){
+    _buildOpts: function (options) {
         var defaultOpts = this._getDefaultHttpOpts();
-		return this.customHttpOptions($.extend({}, defaultOpts, {
+        return this.customHttpOptions($.extend({}, defaultOpts, {
             data: options.data,
             url: options.url,
             type: options.method
         }));
-        
-	},
-	storePromiseData: function(promise, nonce, isBlocking){
-        console.log(promise);
-		var promises = this.unresolvedPromises;
-		promises.push({id: promise._id, nonce: nonce, isBlocking: isBlocking});
-	},
-	unresolvedPromises: null,
-	blocking: false,
-	initialize: Ember.on('init', function(){
-		this.set('unresolvedPromises', []);
-	})
+
+    },
+    storePromiseData: function (promise, nonce, isBlocking) {
+        var promises = this.unresolvedPromises;
+        promises.push({id: promise._id, nonce: nonce, isBlocking: isBlocking});
+    },
+    unresolvedPromises: null,
+    blocking: false,
+    initialize: Ember.on('init', function () {
+        this.set('unresolvedPromises', []);
+    })
 });
